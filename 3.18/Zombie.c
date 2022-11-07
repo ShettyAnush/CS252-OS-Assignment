e372ff56e749be192706fba97c4806aaab24cb07
Anush Babu Shetty <79580543+ShettyAnush@users.noreply.github.com>
2022-11-07 05:51:26 +0530

    Update Zombie.c

--- 3.18/Zombie.c
+++ 3.18/Zombie.c
@@ -1,1 +1,24 @@
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+            // Fork returns process id in parent process
+            int pid = fork();
+            if(pid<0){
+                 //exit(1);
+                printf("failed to fork\n");
+                return -1;
+            }
+            // Parent process
+            if (pid > 0){
+                printf("parent pid: %d\n", getpid());
+                sleep(10);
+            }
+            // Child process
+            else{
+                printf("child pid: %d\n", getpid());
+                exit(0);
+            }
+            return 0;
+}
